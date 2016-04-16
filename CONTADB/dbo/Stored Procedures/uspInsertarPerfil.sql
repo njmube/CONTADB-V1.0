0ebc514bdf3cabc3d2aa255a47acdcd44477671e
bourne17 <eduar2.mariano@gmail.com>
2016-04-16 17:28:36 -0400

    Actualizacion de inicio

---
+++ CONTADB/dbo/Stored Procedures/uspInsertarPerfil.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		EDUARDO MARIANO
+-- Create date: 16/04/2016
+-- Description:	Inserta un nuevo perfil de usuario
+-- =============================================
+CREATE PROCEDURE [dbo].[uspInsertarPerfil]
+            @Descripcion as  varchar(100)
+		   ,@Observaciones as nvarchar(100)
+
+AS
+	INSERT INTO [dbo].[tbPerfiles]
+           ([Descripcion]
+		   ,[Observaciones]
+		   ,[FechaCreacion]
+		   ,[FechaModificacion])
+     VALUES
+           (@Descripcion
+           ,@Observaciones
+           ,GETDATE()
+		   ,GETDATE())
+RETURN 0
---
+++ CONTADB/dbo/Stored Procedures/uspListarPerfilesPorUsuarioID.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		EDUARDO MARIANO
+-- Create date: 94/10/2015
+-- Description:	Lista los perfiles relacionados a los usuarios
+-- =============================================
+CREATE PROCEDURE [dbo].[uspListarPerfilesPorUsuarioID]
+(@UsuarioID int)
+WITH 
+EXECUTE AS CALLER
+AS
+	SELECT tbUsuarios.LoginUsuario AS Usuario,
+       tbPerfiles.Descripcion AS Perfil,
+       tbUsuarioPerfiles.Codigo
+  FROM    (   dbo.tbUsuarioPerfiles tbUsuarioPerfiles
+           INNER JOIN
+              dbo.tbPerfiles tbPerfiles
+           ON (tbUsuarioPerfiles.PerfilID = tbPerfiles.Codigo))
+       INNER JOIN
+          dbo.tbUsuarios tbUsuarios
+       ON (tbUsuarioPerfiles.UsuarioID = tbUsuarios.Codigo)
+WHERE tbUsuarios.Codigo=@UsuarioID
