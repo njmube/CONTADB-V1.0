c9aaf7c21b9058a7da342bb3b7ac99026ed0b8a7
bourne17 <eduar2.mariano@gmail.com>
2016-04-16 18:05:35 -0400

    Actualizacion de Perfiles

---
+++ CONTADB/dbo/Stored Procedures/uspActualizarPerfil.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[uspActualizarPerfil]
+	       (@Codigo AS int
+           ,@Descripcion as  varchar(100)
+           ,@Observaciones as varchar(200))
+
+AS
+	UPDATE [dbo].[tbPerfiles]
+   SET [Descripcion] = @Descripcion
+      ,[Observaciones] = @Observaciones
+	  ,[FechaModificacion]=GETDATE()
+ WHERE Codigo=@Codigo
+RETURN 0
---
+++ CONTADB/dbo/Stored Procedures/uspBuscarPerfilPorID.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		EDUARDO MARIANO
+-- Create date: 16/04/2016
+-- Description:	Realiza una busqueda de perfiles por su codigo unico
+-- =============================================
+CREATE PROCEDURE [dbo].[uspBuscarPerfilPorID]
+	@Codigo Int
+
+AS
+	SELECT [Codigo]
+      ,[Descripcion]
+      ,[Observaciones]
+	  ,[FechaCreacion]
+	  ,[FechaModificacion]
+  FROM [dbo].[tbPerfiles]
+  WHERE Codigo=@Codigo
+RETURN 0
